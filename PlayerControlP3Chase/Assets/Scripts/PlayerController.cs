e3960318f5dc5b642ea21ebe7790ceb87365010f
hyper-sonic1 <737509@student.dusd.net>
2024-03-04 11:49:36 -0800

    My acre can move and turn
    almost done

---
+++ PlayerControlP3Chase/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerCon : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnSpeed = 65.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Move the car forward based on vertal input
+
+        // We'll move the vehical forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+    }
+}
